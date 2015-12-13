9ddaf8fa821cb9eda99f4a5fcecc9d60009d92d5
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2015-12-14 00:48:19 +0100

    Let's see if this version works

--- t/travis.sh
+++ t/travis.sh
@@ -14,5 +14,6 @@ env
 run sboconfig -V 14.1
 run sbosnap fetch
 run sbofind sbotools
-[[ 3 -eq yes | run sboinstall -i sbotools ]]
+INST=$(run sboinstall -i sbotools)
+[[ 3 -eq $? ]]
 sudo t/do_tests.sh
