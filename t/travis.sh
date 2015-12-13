03aa1179a33de74249664428cc87c030ef991175
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2015-12-13 23:41:40 +0100

    Set debugging options and do an sbosnap fetch

--- t/travis.sh
+++ t/travis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Set up tests to run under Travis
+set -e
+set -x
+
+perl -I"SBO-Lib/lib" sbosnap fetch
 
 echo "Not actually testing anything. Just verifying travis runs this."
 exit 0
