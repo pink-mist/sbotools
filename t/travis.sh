70ca75aec86bb955a952ddb573c81e47bedf0f2b
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2015-12-13 23:46:08 +0100

    Let sbosnap fetch be run with sudo

--- t/travis.sh
+++ t/travis.sh
@@ -4,7 +4,8 @@
 set -e
 set -x
 
-perl -I"SBO-Lib/lib" sbosnap fetch
+PERL=`which perl`
+sudo $PERL -I"SBO-Lib/lib" sbosnap fetch
 
 echo "Not actually testing anything. Just verifying travis runs this."
 exit 0
