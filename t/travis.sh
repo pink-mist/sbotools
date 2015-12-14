b888ada8f817326d317775bf17fb230e2ebca4b0
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2015-12-14 14:28:13 +0100

    Test custom repo; see #19

--- t/travis.sh
+++ t/travis.sh
@@ -4,17 +4,25 @@
 set -e
 set -x
 
+CWD=$(pwd)
 PERL=`which perl`
 I="SBO-Lib/lib"
 run() {
-	sudo $PERL "$@"
+	sudo $PERL -I$I "$@"
 }
 
 env
 
-run -I$I sboconfig -V 14.1
-run -I$I sbosnap fetch
-run -I$I sbofind sbotools
+run sboconfig -V 14.1
+run sbosnap fetch
+run sbofind sbotools
 cd t
-run prep.pl
-run test.t
+sudo $PERL prep.pl
+sudo $PERL test.t
+cd $CWD
+sudo rm -rf /usr/sbo
+[ ! -e /usr/sbo/repo/SLACKBUILDS.TXT ]
+run sboconfig -r https://github.com/Ponce/slackbuilds.git
+run sbosnap fetch
+[ -e /usr/sbo/repo/SLACKBUILDS.TXT ]
+[ ! -e /usr/sbo/repo/SLACKBUILDS.TXT.gz ]
